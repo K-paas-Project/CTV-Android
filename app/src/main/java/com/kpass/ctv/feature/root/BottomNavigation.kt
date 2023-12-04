3ac79ea9802a4ba58dd4198503dbc5552fd80860
8954sood <58807462+8954sood@users.noreply.github.com>
2023-12-04 21:37:36 +0900

    리플 범위 조정

--- app/src/main/java/com/kpass/ctv/feature/root/BottomNavigation.kt
+++ app/src/main/java/com/kpass/ctv/feature/root/BottomNavigation.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -49,7 +51,8 @@ fun BottomNavigation(
             CtvBottomButton(
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxHeight(),
+                    .fillMaxHeight()
+                    .clip(RoundedCornerShape(0.dp)),
                 text = item.icName,
                 onClick = {
                     Log.d("TAG", "BottomNavigation: ${item.icName}")
@@ -66,6 +69,7 @@ fun BottomNavigation(
                     }
                 },
                 selected = selectedTab.group == item.group,
+                enable = true,
                 iconId = item.icon
             )
         }
